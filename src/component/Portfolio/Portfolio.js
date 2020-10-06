dbe731fe1d0ad5ff22eea0f2630eb5991ff0da8e
Camille Sylla <camillesylla.web@gmail.com>
2020-10-06 16:28:55 +0200

    create modal and its functionnal

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import useFirestore from '../../Hooks/useFirestore'
+import React, { useState } from 'react';
+
 
 //components
 import Nav from './Nav/Nav';
 import UploadForm from '../UploadForm';
 
-//assets
-import One from '../../assets/portfolio/arash-payam--qtnZj7zB2U-unsplash.jpg'
-
 import '../Portfolio/Portfolio.css'
+import Modal from '../Modal';
+import ImageGrid from './ImageGrid';
 
 
 const Portfolio = () => {
-    const { docs } = useFirestore('images');
-    console.log(docs)
+    
+    const [selectedImg, setSelectedImg] = useState(null)
     return (
         <div className="pCont">
             <Nav />
-            <UploadForm /><div className="img-grid">
-            { docs && docs.map(doc => (
-                    <div className="img-wrap" key={doc.id}>
-                        <img src={doc.url} alt="uploaded pic" />
-                </div>
-            ))}
-            </div>
+            <UploadForm />
+            <ImageGrid setSelectedImg={setSelectedImg}/>
+            { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
         </div>
     )
 }
